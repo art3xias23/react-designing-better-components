e26f82ba5d2ee329fe96ead73294496c4aafbc1f
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-08 17:02:23 +0000

    Moved eventHandler and useEffect from SpeakersList to useStateManager

--- pages/components/SpeakersList.js
+++ pages/components/SpeakersList.js
@@ -1,41 +1,13 @@
 import Speaker from './Speaker'
 import {useStateManager } from '../modules/StateManager'
-import {useEffect} from 'react'
-import {data} from './../SpeakerData'
 import ReactPlaceholder from 'react-placeholder/lib'
 
 export default function SpeakersList({showSessions}){
-  const {speakerData, setSpeakerData, 
-    isLoading, setIsLoading, 
-    hasErrored, setHasErrored,
-  error, setError} = useStateManager();
+  const {speakerData, isLoading,
+    hasErrored, error,
+    onFavoriteToggle} = useStateManager(2000);
 
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-  useEffect(async () => {
-    try{
-    setIsLoading(true);
-    await delay(2000);
-    setSpeakerData(data)
-    setIsLoading(false)
-    }
-    catch(error){
-      setHasErrored(true);
-      setError(error);
-    }
-  },[]);
-
-  const onFavoriteToggle =(speakerId) =>{
-    console.log("re-toggle")
-    const speakerPrevData = speakerData.find(function(x) {return  x.id==speakerId});
-
-    const updatedSpeakerNewData = {...speakerPrevData, favorite: !speakerPrevData.favorite}
-
-    const newSpeakersData = speakerData.map((speaker) => 
-    speaker.id == speakerId ? updatedSpeakerNewData : speaker);
-
-    setSpeakerData(newSpeakersData);
-  }
+  
 
   if(hasErrored) return(
     <div className="text-danger">
--- pages/modules/StateManager.js
+++ pages/modules/StateManager.js
@@ -1,5 +1,6 @@
-import {useState} from 'react'
-export function useStateManager() {
+import {useState, useEffect} from 'react'
+import {data} from './../SpeakerData'
+export function useStateManager(ms) {
 
 const [showSessions, setShowSessions] = useState(true);
   const [theme, setTheme] = useState("light");
@@ -7,11 +8,35 @@ const [showSessions, setShowSessions] = useState(true);
   const [isLoading, setIsLoading] = useState(true)
   const [hasErrored, setHasErrored] = useState(false);
   const [error, setError] = useState();
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  return {showSessions, setShowSessions,
-    theme, setTheme, 
-    speakerData, setSpeakerData,
-  isLoading, setIsLoading,
-hasErrored, setHasErrored,
-error, setError};
+  useEffect(async () => {
+    try{
+    setIsLoading(true);
+    await delay(ms);
+    setSpeakerData(data)
+    setIsLoading(false)
+    }
+    catch(error){
+      setHasErrored(true);
+      setError(error);
+    }
+  },[]);
+
+  const onFavoriteToggle =(speakerId) =>{
+    console.log("re-toggle")
+    const speakerPrevData = speakerData.find(function(x) {return  x.id==speakerId});
+
+    const updatedSpeakerNewData = {...speakerPrevData, favorite: !speakerPrevData.favorite}
+
+    const newSpeakersData = speakerData.map((speaker) => 
+    speaker.id == speakerId ? updatedSpeakerNewData : speaker);
+
+    setSpeakerData(newSpeakersData);
+  }
+
+  return {speakerData, isLoading,
+  hasErrored, error,
+  onFavoriteToggle
+};
 }
