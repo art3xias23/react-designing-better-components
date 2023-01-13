a19dc2e6565344dac958c674f0d7fc7bb7ca8db6
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-13 07:59:43 +0000

    Added api id route and put, delete and post methods

---
+++ pages/api/speakers/[id].js
@@ -0,0 +1,112 @@
+//import {initalData as data} from '../../../data'
+import path from "path";
+import fs from "fs";
+
+const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+const delay = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+
+export default async function handler(req, res) {
+  console.log("API request made");
+  console.log(JSON.stringify(data, null, 2));
+  // res.status(200).send(JSON.stringify(data, null, 2));
+
+  const method = req?.method;
+  const id = parseInt(req?.query.id);
+  const recordFromBody = req?.body;
+  const jsonFile = path.resolve("./", "db.json");
+
+  switch (method) {
+    case "POST":
+      await postMethod();
+      break;
+    case "PUT":
+      await putMethod();
+      break;
+    case "DELETE":
+      await deleteMethod();
+      break;
+    default:
+      res.status(501).send(`Method ${method} not implemented`);
+  }
+
+  async function postMethod() {
+    try {
+      const readFileData = await readFile(jsonFile);
+      await delay(1000);
+      const speakers = JSON.parse(readFileData).speakers;
+
+      if(!speakers){
+        res.status(404).send("Error: Request failed with status code 404");
+      } else{
+        const newId = speakers.reduce((accumulator, currentValue) => {
+            const idCurrent = parseInt(currentValue.Id);
+            return idCurrent > accumulator ? idCurrent : accumulator;
+        }, 0) + 1;
+
+        const newRecord = {...recordFromBody, id: newId};
+        const newSpeakersArray = [newRecord, ...speakers];
+
+            writeFile(jsonFile, JSON.stringify({speakers: newSpeakersArray}, null, 2));
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify(newRecord, null, 2));
+        console.log(`POST /api/speakers/{$id} status: 200`);
+      }
+
+    } catch (e) {
+      console.log("/api/speakers error", e);
+      res.status(404).send("File Not Found on server");
+    }
+  }
+
+  async function putMethod() {
+    try {
+      const readFileData = await readFile(jsonFile);
+      await delay(1000);
+      const speakers = JSON.parse(readFileData).speakers;
+
+      if(!speakers){
+        res.status(404).send("Error: Request failed with status code 404");
+      } else{
+        const newSpeakersArray = speakers.map((rec) => rec.id == id ? recordFromBody : rec);
+
+            writeFile(jsonFile, JSON.stringify({speakers: newSpeakersArray}, null, 2));
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify(recordFromBody, null, 2));
+        console.log(`POST /api/speakers/{$id} status: 200`);
+      }
+
+    } catch (e) {
+      console.log("/api/speakers error", e);
+      res.status(404).send("File Not Found on server");
+    }
+  }
+
+  async function deleteMethod() {
+    try {
+      const readFileData = await readFile(jsonFile);
+      await delay(1000);
+      const speakers = JSON.parse(readFileData).speakers;
+
+      if(!speakers){
+        res.status(404).send("Error: Request failed with status code 404");
+      } else{
+        const newSpeakersArray = speakers.map((rec) => rec.id != id);
+
+            writeFile(jsonFile, JSON.stringify({speakers: newSpeakersArray}, null, 2));
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify(speakers.find((rec) => rec.id == id), null, 2));
+        console.log(`DELETE /api/speakers/{$id} status: 200`);
+      }
+
+    } catch (e) {
+      console.log("/api/speakers error", e);
+      res.status(404).send("File Not Found on server");
+    }
+  }
+}
--- pages/api/speakers/index.js
+++ pages/api/speakers/index.js
@@ -1,9 +1,32 @@
-import {data} from '../../data'
+//import {initialData as data} from '../../../data'
+import path from "path";
+import fs from "fs";
 
-export default function handler(req, res){
-    console.log("API request made")
-    res.status(200).json(JSON.stringify(data, null, 2));
-    console.log(`response`)
-    console.dir(res)
+const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
 
+const delay = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+
+export default async function handler(req, res) {
+  console.log("API request made");
+  console.log(JSON.stringify(data, null, 2));
+  // res.status(200).send(JSON.stringify(data, null, 2));
+
+  const jsonFile = path.resolve("./", "db.json");
+
+  try {
+    const readFileData = await readFile(jsonFile);
+    await delay(1000);
+    const speakers = JSON.parse(readFileData).speakers;
+    if (speakers) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(speakers, null, 2));
+    }
+  } catch (e) {
+    console.log("/api/speakers error", e);
+    res.status(404).send("File Not Found on server");
+  }
 }
