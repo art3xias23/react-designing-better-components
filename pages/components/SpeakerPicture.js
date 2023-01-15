47035c409a3dcdfc9c1272fa7710bd3e25ef3236
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-15 19:55:53 +0000

    Added speaker image component specifically for the image and updated gitignore for db.json

--- pages/components/SpeakerPicture.js
+++ pages/components/SpeakerPicture.js
@@ -1,11 +1,24 @@
 import { SpeakerContext } from "../contexts/SpeakerContext";
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 
+
+function SpeakerWithImage({src, ...props}){
+  const [error, setError] = useState(false);
+  const [imgSrc, setImgSrc] = useState(src);
+
+  function onError(){
+    if (!error){
+      setImgSrc("/images/speaker-99999.jpg");
+      setError(true);
+    }
+  }  
+   return <img src={imgSrc} {...props} onError={onError} />
+}
 export default function SpeakerPicture() {
   const {speaker: {id, first, last}} = useContext(SpeakerContext);
   return (
     <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
-      <img
+      <SpeakerWithImage
         className="contain-fit"
         src={`/images/speaker-${id}.jpg`}
         width="300"
