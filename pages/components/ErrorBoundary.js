7ca4478a3268daa2ddcb72d4d364496525f73580
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-17 08:59:21 +0000

    Added custom ui for error boundary

--- pages/components/ErrorBoundary.js
+++ pages/components/ErrorBoundary.js
@@ -1,16 +1,26 @@
+import React from "react";
 class ErrorBoundary extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { hasError: false };
-    }
-  
-    static getDerivedStateFromError(error) {    // Update state so the next render will show the fallback UI.    
-        return { hasError: true };  }
-    componentDidCatch(error, errorInfo) {    // You can also log the error to an error reporting service    
-        logErrorToMyService(error, errorInfo);  }
-    render() {
-      if (this.state.hasError) {      // You can render any custom fallback UI      
-        return <h1>Something went wrong.</h1>;    }
-      return this.props.children; 
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
   }
+
+  static getDerivedStateFromError(error) {
+    console.log("ErrorBoundary:getDerivedStateFromError");
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {} // You can also log the error
+
+  render() {
+    console.log("ERRORBOUNDARY render");
+    if (this.state.hasError) {
+      if (this.props.errorUI) {
+        return this.props.errorUI;
+      }
+      return <div>Something went wrong</div>;
+    }
+    return this.props.children;
+  }
+}
+export default ErrorBoundary;
--- pages/components/Speaker.js
+++ pages/components/Speaker.js
@@ -3,38 +3,39 @@ import SpeakerInfo from "./SpeakerInfo";
 import SpeakerPicture from "./SpeakerPicture";
 import { useContext, memo } from "react";
 import { SpeakerFilterContext } from "../../contexts/SpeakerFilterContext";
-import SpeakerDelete from './SpeakerDelete';
-import ErrorBoundary from './ErrorBoundary';
+import SpeakerDelete from "./SpeakerDelete";
+import ErrorBoundary from "./ErrorBoundary";
 
 const SpeakerNoErrorBoundary = memo(function Speaker({
   speaker,
   updateRecord,
-  deleteRecord
+  deleteRecord,
 }) {
   const { showSessions } = useContext(SpeakerFilterContext);
 
   return (
-      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
-        <div className="card card-height p-4 mt-4">
-          <SpeakerPicture />
-          <SpeakerInfo />
-        </div>
-        {showSessions && <SpeakerSessions />}
-        <SpeakerDelete />
+    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+      <div className="card card-height p-4 mt-4">
+        <SpeakerPicture />
+        <SpeakerInfo />
       </div>
+      {showSessions && <SpeakerSessions />}
+      <SpeakerDelete />
+    </div>
   );
-}, areEqualSpeaker);
+},
+areEqualSpeaker);
 
-function Speaker(props){
-  return(
-    <ErrorBoundary>
-      <SpeakerNoErrorBoundary {...props}></SpeakerNoErrorBoundary>
-    </ErrorBoundary>
-  )
+function areEqualSpeaker(prevProps, nextProps) {
+  return prevProps.speaker.favorite == nextProps.speaker.favorite;
 }
 
-function areEqualSpeaker(prevProps, nextProps){
-  return (prevProps.speaker.favorite == nextProps.speaker.favorite);
+function Speaker(props) {
+  return (
+    <ErrorBoundary errorUI={<div>Somethin went wrong in the speaker card</div>}>
+      <SpeakerNoErrorBoundary {...props}></SpeakerNoErrorBoundary>
+    </ErrorBoundary>
+  );
 }
 
 export default Speaker;
