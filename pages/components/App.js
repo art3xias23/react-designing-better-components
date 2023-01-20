c4d456a0cfee9c31e79880ea05be38913cee98ab
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-20 21:42:36 +0000

    Added Auth HOC

--- pages/components/App.js
+++ pages/components/App.js
@@ -1,12 +1,15 @@
 import Header from "./Header";
 import Speakers from "./Speakers";
 import Layout from "./Layout";
+import { AuthProvider } from "../../contexts/AuthContext";
 
 export default function App() {
   return (
+    <AuthProvider initialLoggedInUser={"Ronald"}>
       <Layout>
         <Header />
         <Speakers />
       </Layout>
+    </AuthProvider>
   );
 }
